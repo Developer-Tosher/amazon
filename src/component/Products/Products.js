428b2c61837f2f9341521fd627497b4e1eed28e4
masternati <sohrabhossain207@gmail.com>
2024-06-29 14:39:23 +0600

    Items added with three column

--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import './Products.css'
+import './Products.css';
 
 const Products = ({ product }) => {
     return (
         <div className='product'>
-            <img src={product.img} alt='image'/>
-            <h3>{product.name}</h3>
-            <p>Price: ${product.price}</p>
-            <p>Manufacturer: {product.seller}</p>
-            <p>Ratting: {product.ratting}</p>
-
-            <button>Add To Cart</button>
+            <div>
+                <div className='product-image'>
+                    <img src={product.img} alt='product image' />
+                </div>
+                <div className='product-details'>
+                    <div className='show-info'>
+                        <h3>{product.name}</h3>
+                        <p>Price: ${product.price}</p>
+                        <p>Manufacturer: {product.seller}</p>
+                        <p>Rating: {product.rating}</p>
+                        <button>Add To Cart</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
